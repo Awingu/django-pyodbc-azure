bc22c04fd5ca454d2cb632e46977ae2e6a7681b5
Ruben De Visscher <ruben@rubendv.be>
2018-04-24 09:21:48 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ CLASSIFIERS=[
 
 setup(
     name='django-pyodbc-azure',
-    version='2.0.4.1awingu1',
+    version='2.0.4.1awingu2',
     description='Django backend for Microsoft SQL Server and Azure SQL Database using pyodbc',
     long_description=open('README.rst').read(),
     author='Michiya Takahashi',
