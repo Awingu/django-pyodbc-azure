ab1c79c94df1dc5dfd996329d8db1aa2307442b3
Ruben De Visscher <ruben@awingu.com>
2020-05-26 14:10:28 +0200

    Add support for MSSQL v15 (2019). JIRA: DEV-8415

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ CLASSIFIERS=[
 
 setup(
     name='django-pyodbc-azure',
-    version='2.1.0.0awingu2',
+    version='2.1.0.0awingu3',
     description='Django backend for Microsoft SQL Server and Azure SQL Database using pyodbc',
     long_description=open('README.rst').read(),
     author='Michiya Takahashi',
